46914b627a6e99d23312b762b97fad69cc2d6cda
LazezBZH <lazez.bzh@caramail.fr>
2021-09-15 12:45:12 +0200

    V05 page About

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "./Collapse.css"
+
+class Collapse extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isClosed: true }
+    this.openCollapse = this.openCollapse.bind(this)
+  }
+
+  openCollapse() {
+    this.setState({ isClosed: !this.state.isClosed })
+  }
+
+  render() {
+    return (
+      <div
+        className={`collapse collapse${
+          this.state.isClosed ? "-closed" : "-open"
+        }`}
+      >
+        <div className="collapse-title" onClick={this.openCollapse}>
+          <h2>{this.props.title}</h2>
+          <i
+            className={`fas  ${
+              this.state.isClosed ? "fa-chevron-up" : "fa-chevron-down"
+            }`}
+          ></i>
+        </div>
+        <p
+          className={`collapse-content${
+            this.state.isClosed ? "_closed" : "_open"
+          }`}
+        >
+          {this.props.content}
+        </p>
+      </div>
+    )
+  }
+}
+
+Collapse.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]).isRequired,
+}
+
+export default Collapse
